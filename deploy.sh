bca7111d1bc571c2ce37642752755bff412c2357
Daniel Rädler <daniel.raedler@web.de>
2021-03-19 10:39:54 +0100

    fix script missing apply

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ docker push raedlerdan/multi-client:$GIT_SHA
 docker push raedlerdan/multi-server:$GIT_SHA
 docker push raedlerdan/multi-worker:$GIT_SHA
 
-kubectl -f k8s
+kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=raedlerdan/multi-server:$GIT_SHA
 kubectl set image deployments/client-deployment client=raedlerdan/multi-client:$GIT_SHA
 kubectl set image deployments/worker-deployment worker=raedlerdan/multi-worker:$GIT_SHA
